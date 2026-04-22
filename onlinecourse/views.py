a097f70b2b6a468a919346e94919b7bc551d6dd8
caleboppong <maroppcal@hotmail.it>
2026-04-22 06:07:37 -0400

    Complete Task 5 and Task 6 exam submission and result views

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -135,15 +135,22 @@ def show_exam_result(request, course_id, submission_id):
     selected_ids = [choice.id for choice in selected_choices]
 
     total_score = 0
+    total_possible = 0
 
     for question in course.question_set.all():
+        total_possible += question.grade
         if question.is_get_score(selected_ids):
             total_score += question.grade
+
+    if total_possible > 0:
+        grade = int(total_score / total_possible * 100)
+    else:
+        grade = 0
 
     context = {
         'course': course,
         'submission': submission,
-        'grade': total_score,
+        'grade': grade,
     }
 
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
